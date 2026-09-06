e979c01e290be3ef0b926f113b09e8658457bfee
asck888 <147483679+asck888@users.noreply.github.com>
2026-09-06 15:19:31 +0800

    Fix awk insertion

--- apply_device.sh
+++ apply_device.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 cd openwrt
-# Insert device definition before the BuildImage eval line
-awk '/\$\(eval \$\(call BuildImage\)\)/{while((getline line < "../dts/en7528-ha030wc.mk") > 0) print line}1' target/linux/econet/image/en7528.mk > /tmp/en7528_new.mk
-cp /tmp/en7528_new.mk target/linux/econet/image/en7528.mk
+# Insert device definition before last line (BuildImage eval)
+total=$(wc -l < target/linux/econet/image/en7528.mk)
+head -n $((total - 1)) target/linux/econet/image/en7528.mk > /tmp/part1.mk
+cat ../dts/en7528-ha030wc.mk >> /tmp/part1.mk
+tail -n 1 target/linux/econet/image/en7528.mk >> /tmp/part1.mk
+cp /tmp/part1.mk target/linux/econet/image/en7528.mk
 echo "=== Check ==="
 grep -c "nokia_ha030wc" target/linux/econet/image/en7528.mk
